8c26579718b671722fd4b3c44139eec0882a760a
Ana Claudia Martins Ciconelle <acmarc@bootes.ime.usp.br>
2019-10-21 13:55:25 -0300

    adicionando gender and age

--- analise_descritiva.R
+++ analise_descritiva.R
@@ -3,6 +3,8 @@ dados = read.table("noshowappointments/KaggleV2-May-2016.csv", stringsAsFactors 
 head(dados)
 attach(dados)
 
+
+##### Analise do tempo de espera entre o agendamento e consulta #####
 dados$ScheduledDay
 dados$AppointmentDay
 
@@ -22,4 +24,48 @@ dia
 tempo = difftime(dia,agenda,units = "day")
 tempo = as.integer(tempo)
 
-which(tempo<0)
+#Amostras com erro 
+dados$ScheduledDay[which(tempo<0)] = NA
+dados$AppointmentDay[which(tempo<0)] = NA
+
+#Distribuicao do tempo
+boxplot(tempo~No.show)
+hist(tempo,freq = F, col="green", nc=30)
+
+
+##### Genero #####
+colnames(dados)
+
+table(Gender)
+
+barplot(table(Gender))
+barplot(table(Gender,No.show), beside = T)
+
+##### Age #####
+
+table(Age)
+
+Age[which(Age<0)] = NA
+
+barplot(table(Age))
+
+faixas = c("0-5","6-18","19-60","61+")
+
+bebe = which(Age<6)
+crianca = which(Age>5 & Age<19)
+adulto = which(Age>18 & Age<61)
+idoso = which(Age>60)
+
+barplot(c(length(bebe), length(crianca), length(adulto),length(idoso))/length(Age), 
+        names = faixas)
+
+faixa_etaria = rep(NA, length(Age))
+faixa_etaria[bebe] = "Bebe"
+faixa_etaria[crianca] = "Criança"
+faixa_etaria[adulto] = "Adulto"
+faixa_etaria[idoso] = "Idoso"
+
+barplot(table(No.show,faixa_etaria), beside = T)
+barplot(t(t(table(No.show,faixa_etaria))/apply(table(No.show,faixa_etaria),2,sum)), beside = T)
+
+
