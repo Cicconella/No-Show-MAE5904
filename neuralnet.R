b11882c5a192a9456dbac0179aef8a26f14c469e
cicconella <acmarc@gmail.com>
2019-11-03 13:21:55 -0300

    neural net code

---
+++ neuralnet.R
@@ -0,0 +1,90 @@
+################# nnet
+
+head(dados)
+
+require(nnet)
+
+model_nnet <- nnet(formula = No.show ~ Gender + Age + Neighbourhood + Scholarship +
+                     Hipertension + Diabetes + Alcoholism + Handcap + SMS_received +
+                     Hour + Day_Week + Day_Week_Appointment, 
+                   data=dados, size=4, decay=0.0001, maxit = 700, 
+                   trace= FALSE)
+
+#summary(model_nnet)
+x = dados[,c(3:11,13:16)]
+y = dados[,12]
+
+pred <- predict(model_nnet,x,type="class") 
+resultado <- confusionMatrix(as.factor(pred), y)
+resultado
+
+
+head(dados)
+
+yes=which(dados$No.show=="Yes")
+length(yes)
+
+no=which(dados$No.show=="No")
+length(no)
+
+dados2=dados[c(yes,no[1:length(yes)]),]
+dim(dados2)
+
+model_nnet2 <- nnet(formula = No.show ~ Gender + Age + Neighbourhood + Scholarship +
+                     Hipertension + Diabetes + Alcoholism + Handcap + SMS_received +
+                     Hour + Day_Week + Day_Week_Appointment, 
+                   data=dados2, size=4, decay=0.0001, maxit = 700, 
+                   trace= FALSE)
+
+#summary(model_nnet)
+x2 = dados2[,c(3:11,13:16)]
+y2 = dados2[,12]
+
+pred2 <- predict(model_nnet2,x2,type="class") 
+resultado2 <- confusionMatrix(as.factor(pred2), y2)
+resultado2
+
+#### 
+
+teste = read.table("teste.csv", header=T, sep=",", stringsAsFactors = T)
+head(teste)
+summary(teste)
+
+
+
+
+# #install.packages("neuralnet")
+# library(neuralnet)
+# 
+# dados = read.table("treinamento.csv", header=T, sep=",", stringsAsFactors = T)
+# head(dados)
+# summary(dados)
+# attach(dados)
+# 
+# dados$No.show 
+# dados$No.show [which(dados$No.show =="Yes")] = 1 
+# dados$No.show [which(dados$No.show =="No")] = 0 
+# dados$No.show = as.numeric(dados$No.show)
+# 
+# dados$Gender[which(dados$Gender=="F")] = 0
+# dados$Gender[which(dados$Gender=="M")] = 1
+# dados$Gender = as.numeric(dados$Gender)
+# dados$Gender
+# 
+# dados$Gender = as.numeric(dados$Gender) 
+# dados$Age = as.numeric(dados$Age) 
+# dados$Scholarship = as.numeric(dados$Scholarship) 
+# 
+# # +Age+Neighbourhood+Scholarship+Hipertension+Diabetes+Alcoholism+Handcap 
+# # +SMS_received+Wait+Hour+Day_Week + Day_Week_Appointment
+# 
+# model_nn <- neuralnet(formula = No.show ~ Gender+Age+Scholarship,
+#                       data=dados, hidden=1, rep=1, err.fct = "ce", linear.output = FALSE)
+# model_nn
+# 
+# final_output=cbind (dados$No.show, 
+#                     as.data.frame(model_nn$net.result) )
+# print(final_output)
+
+
+
