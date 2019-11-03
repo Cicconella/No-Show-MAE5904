c4b1c030eaf045b0e69d6bcaa5e7a46e2a0bf740
RafaelOliv3ira <rafaeloliv3ira@gmail.com>
2019-11-03 00:08:46 -0300

    legenda do gráfico

--- Arvore_clasificacao.R
+++ Arvore_clasificacao.R
@@ -5,30 +5,32 @@ library(dplyr)
 library(xtable)
 require(plyr)
 
-dados.trat <- read.table("treinamento.csv", header=T, sep=",")
+dados.trat <- read.table("treinamento.csv", header=T, sep=",") %>% mutate(No.show = ifelse(No.show=="Yes","No-Show","Show")) 
 dim(dados.trat)
 dados.trat <- dados.trat[-which(is.na(dados.trat$Wait)),]
 dim(dados.trat)
-dados.test <- read.table("teste.csv", header=T, sep=",")
+dados.test <- read.table("teste.csv", header=T, sep=",") %>% mutate(No.show = ifelse(No.show=="Yes","No-Show","Show")) 
 dim(dados.test)
 which(is.na(dados.test$Wait))
 
 data <- rbind.data.frame(dados.trat,dados.test)
 
+
+table(data$No.show)
 dim(data)
 
 head(data)
 attach(data)
 
+table(data$No.show)
 
 tree.no_show <- rpart(formula = No.show ~ Gender + Age  + Scholarship + Hipertension + Diabetes + Alcoholism + Handcap
-                      + SMS_received + Wait, data = data,method = "class", maxdepth =3,
-                      minsplit = 3,
-                      minbucket = 2,
+                      + SMS_received + Wait, data = data,method = "class",maxdepth =3,
+                      minsplit = 2,
+                      minbucket = 1,
                       cp=-1)
 
-rpart.plot::rpart.plot(tree.no_show, cex = 0.9)
-
+rpart.plot(tree.no_show, cex = 0.9)
 tree.no_show 
 tree.no_show$cptable
 
@@ -36,7 +38,7 @@ table(predict(tree.no_show,type = "class"))
 
 
 tree.no_show.trat <-  rpart(formula = No.show ~ Gender + Age  + Scholarship + Hipertension + Diabetes + Alcoholism + Handcap
-                            + SMS_received + Wait, data = dados.trat,method = "class",maxdepth =4,
+                            + SMS_received + Wait, data = dados.trat,method = "class",maxdepth =3,
                             minsplit = 2,
                             minbucket = 1,
                             cp=-1)
